513d46b4df436ff6de19e3fd49c168404019f474
ciamac-da <ciamac.davoudi@gmail.com>
2021-02-03 20:18:59 +0100

    Verified by Cia v1.0.4 Implimenting sass/Bulma

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,8 @@ export default {
 </script>
 
 <style>
+@import "../node_modules/bulma/bulma.sass";
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
