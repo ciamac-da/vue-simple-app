eaf84b238bd2293f73c70f0498a91a21c8cac5f2
ciamac-da <ciamac.davoudi@gmail.com>
2021-02-03 21:04:50 +0100

    Verified by Cia v1.0.5 hmmmm Bulma is down

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,34 @@
 <template>
   <div id="app">
+    <div class="nav has-shadow">
+      <div class="container">
+        <div class="nav-left">
+          <a href="" class="nav-item">My Company</a>
+        </div>
+        <span class="nav-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+        <div class="nav-right nav-menu">
+          <router-link to="/" class="nav-item r-item">Home</router-link>
+          <router-link to="faq" class="nav-item r-item">Features</router-link>
+          <router-link to="faq" class="nav-item r-item">About</router-link>
+          <router-link to="faq" class="nav-item r-item">FAQ</router-link>
+
+          <div class="nav-item">
+            <p class="control">
+              <a href="" class="button is-primary is-outlined">
+              <span class="icon">
+                <i class="fa fa-download"></i>
+              </span>
+              <span>Join Now</span>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
     <img src="./assets/logo.png">
     <router-view/>
   </div>
